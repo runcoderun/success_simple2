03a92bc56b4035c430557166a8542c51a16c61c0
Rob Sanheim <rsanheim@gmail.com>
2008-08-05 12:08:11 -0400

    passing

--- test/unit/foo_test.rb
+++ test/unit/foo_test.rb
@@ -2,8 +2,7 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class FooTest < ActiveSupport::TestCase
   def test_requires_baz
-    assert Foo.create.valid?
+    assert !Foo.create.valid?
     assert Foo.create(:baz => "testing validations is awesome!!").valid?
-    
   end
 end
