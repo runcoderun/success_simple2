547c3b6bd89143efb37dadc25b0cbafb1e3e78fa
Rob Sanheim <rsanheim@gmail.com>
2009-02-24 14:45:01 -0500

    fix it

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -2,6 +2,6 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def test_user_requires_name
-    assert_equal true, User.new.valid?
+    assert_equal false, User.new.valid?
   end
 end
